5803aaa8aa13f5914fd47aa74f5104e960886e44
Mario <mario@Marios-MacBook-Air.local>
2022-12-28 18:12:28 -0600

    data

--- script.js
+++ script.js
@@ -28,3 +28,83 @@ $(document).ready(function () {
   }
 
   loadMostRecent()
+
+  //function to load recently searched cities from local storage
+  function loadRecentCities() {
+    let recentCities = JSON.parse(localStorage.getItem("cities"));
+
+    if (recentCities) {
+      cities = recentCities;
+    } else {
+      cities = [];
+    }
+  }
+
+  loadRecentCities()
+
+  //event handler for search city button
+  $("#submit").on("click", (e) => {
+    e.preventDefault();
+    getCity();
+    search();
+    $("#city-input").val("");
+    listCities();
+  });
+
+  //function to save searched cities to local storage
+  function saveToLocalStorage() {
+    localStorage.setItem("mostRecent", city);
+    cities.push(city);
+    localStorage.setItem("cities", JSON.stringify(cities));
+  }
+
+
+  //function to retrieve user inputted city name
+  function getCity() {
+    city = $("#city-input").val();
+    if (city && cities.includes(city) === false) {
+      saveToLocalStorage();
+      return city;
+    } else if (!city) {
+      alert("Please enter a valid city");
+    }
+  }
+
+
+  // searches the API for the chosen city
+  function search() {
+    
+    let queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=0c89982ef24e1c248ccce81e2ec1dc9a";
+    let coords = [];
+
+    $.ajax({
+        url: queryURL,
+        method: "GET",
+      }).then(function (response) {
+        
+        coords.push(response.coord.lat);
+        coords.push(response.coord.lon);
+        let cityName = response.name;
+        let cityCond = response.weather[0].description.toUpperCase();
+        let cityTemp = response.main.temp;
+        let cityHum = response.main.humidity;
+        let cityWind = response.wind.speed;
+        let icon = response.weather[0].icon;
+        $("#icon").html(
+          `<img src="http://openweathermap.org/img/wn/${icon}@2x.png">`
+        );
+        $("#city-name").html(cityName + " " + "(" + NowMoment + ")");
+        $("#city-cond").text("Current Conditions: " + cityCond);
+        $("#temp").text("Current Temp (F): " + cityTemp.toFixed(1));
+        $("#humidity").text("Humidity: " + cityHum + "%");
+        $("#wind-speed").text("Wind Speed: " + cityWind + "mph");
+        $("#date1").text(day1);
+        $("#date2").text(day2);
+        $("#date3").text(day3);
+        $("#date4").text(day4);
+        $("#date5").text(day5);
+  
+        getUV(response.coord.lat, response.coord.lon);
+      }).fail(function (){
+        alert("Could not get data")
+      });
